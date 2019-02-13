a418bda5bd74762e4505370d7149bbfb1772759d
frolushka <mpfrolovstud@gmail.com>
2019-02-13 11:45:16 +0300

    [MOVE] get_next_line to sided repo

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbednar <sbednar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/28 23:18:53 by sbednar           #+#    #+#             */
+/*   Updated: 2019/01/27 05:48:46 by sbednar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+# include <fcntl.h>
+# include <unistd.h>
+
+# define EOF (-1)
+# define BUFF_SIZE 32
+
+int	get_next_line(int const fd, char **line);
+
+#endif
